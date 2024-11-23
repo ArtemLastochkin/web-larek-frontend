de369c993a878d92257307d714136dfb41fb123f
Artem Lastochkin <pryanikov010@yandex.ru>
2024-11-23 12:39:08 +0300

    Добавлена документация проекта в README

---
+++ src/types/index.ts
@@ -0,0 +1,53 @@
+// type Card = {
+// 	id: string;
+// 	description: string;
+// 	image: string;
+// 	title: string;
+// 	category: string;
+// 	price: number | null;
+// };
+
+// interface ICard extends Card {
+// 	cards: Card[];
+// 	getDataCard(id: number): Card;
+// 	getElementCard(id: number): HTMLLIElement;
+// }
+
+// interface ICardUi {
+// 	exampleCard: HTMLLIElement;
+// }
+
+// interface IModalWindow {
+// 	openPopup(card: ICardUi): void;
+// 	closePopup(element: ICardUi): void;
+// }
+
+// interface IPreview {
+// 	cardData: Card;
+// 	id: number;
+// }
+
+// interface IBasket {
+// 	counterMain: number;
+// 	productList: Card[];
+// 	sum: number;
+// 	addElement(cardData: Card): void;
+// 	removeElement(id: number): void;
+// 	removeElementAll(): void;
+// }
+
+// interface IPayment {
+// 	address: string;
+// 	onlinePayment: Boolean;
+// }
+
+// interface IContacts {
+// 	email: string;
+// 	phoneNumber: number;
+// }
+
+// interface ISuccess {
+// 	sum: number
+// } 
+
+
